b56b645c33db2da705a48e7b82495629d8ee490e
Marcel Fahle <m.fahle@gmail.com>
2012-10-07 09:56:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinygallery::Application.config.secret_token = '399e9d7d9d78a436158da953a06cbe0e6dafb65b61804ddaf80d97a71ec47dc21cb3fd6a2b80288979beebb986f0d92a90ff35ed60f6792919cc1beeaffffbe4'
